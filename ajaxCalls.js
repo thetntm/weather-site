22b84e8aca662c4a88704a1f8f51a65f3489b459
thetntm <michael.becker.123@gmail.com>
2020-02-07 22:09:41 -0800

    added Jquery spans and ids

---
+++ ajaxCalls.js
@@ -0,0 +1,68 @@
+//GLOBAL VARS
+
+//LOCAL STORAGE
+
+//DOM ELEMENTS
+
+//  Main Info
+var jq_city_name = $("#city_name")
+
+var jq_current_date = $("#current_date")
+
+var jq_current_temp = $("#current_temp")
+
+var jq_current_humidity = $("#current_humidity")
+
+var jq_current_wind_speed = $("#current_wind_speed")
+
+var jq_current_uv_index = $("#current_uv_index")
+
+//  5 Day Forecast
+
+//      Dates
+var jq_fiveDay_dates = 
+[
+    $("#fiveDay_date1"),
+    $("#fiveDay_date2"),
+    $("#fiveDay_date3"),
+    $("#fiveDay_date4"),
+    $("#fiveDay_date5")
+];
+
+//      Icons
+var jq_fiveDay_icons = 
+[
+    $("#fiveDay_icon1"),
+    $("#fiveDay_icon2"),
+    $("#fiveDay_icon3"),
+    $("#fiveDay_icon4"),
+    $("#fiveDay_icon5")
+]
+
+//      Temp
+var jq_fiveDay_temps = 
+[
+    $("#fiveDay_temp1"),
+    $("#fiveDay_temp2"),
+    $("#fiveDay_temp3"),
+    $("#fiveDay_temp4"),
+    $("#fiveDay_temp5")
+]
+
+var jq_fiveDay_humidity = 
+[
+    $("#fiveDay_humidity1"),
+    $("#fiveDay_humidity2"),
+    $("#fiveDay_humidity3"),
+    $("#fiveDay_humidity4"),
+    $("#fiveDay_humidity5")
+]
+
+//USEFUL FUNCTIONS
+
+//EVENT FUNCTIONS
+
+//EVENT ASSIGNMENT
+
+//CODE TO RUN AT LAUNCH
+jq_current_uv_index.css("background-color","red")
